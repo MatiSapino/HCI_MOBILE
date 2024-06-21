9a73e233b889a8c1d1ec27a267c254e322da6db0
manuader <adermanu@gmail.com>
2024-06-21 19:24:29 -0300

    icons bug fix

--- app/src/main/java/com/example/mobileapp/ui/components/DeviceTypeSection.kt
+++ app/src/main/java/com/example/mobileapp/ui/components/DeviceTypeSection.kt
@@ -40,6 +40,7 @@ fun DeviceTypeSection(
         R.drawable.ac,
         R.drawable.vacuum,
         R.drawable.tap,
+        R.drawable.tap, // Change to Door icon
     )
     Text(
         text = "Types of Devices",
